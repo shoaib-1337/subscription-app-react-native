298208628e79a844edb1c36caa2f9066cb020b69
Shoaib Ali <sali18089@gmail.com>
2026-04-18 21:14:12 +0500

    Added tab style naviation

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,11 +1,13 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { styled } from "nativewind";
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView)
 
 const insights = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-background p-5">
       <Text>insights</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
